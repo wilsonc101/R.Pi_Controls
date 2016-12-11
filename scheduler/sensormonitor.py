326bd0d24c00b26ce1d66e9c6a4d41163dd5f5f0
root <wilsonc101@gmail.com>
2016-12-11 19:03:33 +0000

    bug fix and graph test

--- scheduler/sensormonitor.py
+++ scheduler/sensormonitor.py
@@ -5,6 +5,7 @@ from subprocess import check_output
 
 import core.Adafruit_MAX31855.MAX31855 as MAX31855
 import core.redisdb as redisdb
+import core.config as config
 
 
 class SensorMonitor():
@@ -16,12 +17,12 @@ class SensorMonitor():
         self.uptime = None
         self.cpu_temp = None
 
-        # Setup SQLite Connection
-        self.db_conn = sqlite3.connect('db/pi_control.db')
+        # Setup SQLite Connection - db name and path is fixed
+        self.db_conn = sqlite3.connect(config.local_path + '/db/pi_control.db')
         self.db_cur = self.db_conn.cursor()
-        self.db_cur.execute("CREATE TABLE IF NOT EXISTS sensor_data (TC_internal, TC_external)")
+        self.db_cur.execute("CREATE TABLE IF NOT EXISTS sensor_data (TC_internal, TC_external, record_date)")
 
-        self.temp_data_sql = "INSERT INTO sensor_data(TC_internal, TC_external) VALUES(?, ?) "
+        self.temp_data_sql = "INSERT INTO sensor_data(TC_internal, TC_external, record_date) VALUES(?, ?, ?)"
         self._db_check = False
 
         # Setup sensors/common items
@@ -50,33 +51,44 @@ class SensorMonitor():
             self.uptime = check_output(self.cmd_uptime).decode("utf-8").lstrip().replace("up ", "").replace(", ", "\n")
 
             # Get thermocouple Temps (C)
-            while self.thermo_external_reading == None and self.thermo_internal_reading == None:
+            external_reading = None
+            internal_reading = None
+            while external_reading is None or internal_reading is None:
                 external_reading = self.temp_sensor.readTempC()
                 internal_reading = self.temp_sensor.readInternalC()
-                time.sleep(0.1)
 
-                # filter out bad data
-                if str(external_reading) != "nan" and str(internal_reading) != "nan":
-                    self.thermo_external_reading = "{0:0.2F}'C".format(external_reading)
-                    self.thermo_internal_reading = "{0:0.2F}'C".format(internal_reading)
+                # hold in loop if temp data ia bad
+                if str(external_reading) == "nan" or str(internal_reading) == "nan":
+                    external_reading = None
+                    internal_reading = None
+                    time.sleep(0.1)
 
-            self.logfile.debug("Writing sensor data")
+            self.thermo_external_reading = "{0:0.2F}'C".format(external_reading)
+            self.thermo_internal_reading = "{0:0.2F}'C".format(internal_reading)
+
+            self.logfile.debug("Writing sensor data to store")
             redisdb.setObject("sensor_1", self.pi_cpu_temp)
             redisdb.setObject("sensor_2", self.thermo_internal_reading)
             redisdb.setObject("sensor_3", self.thermo_external_reading)
             redisdb.setObject("sensor_4", self.uptime)
 
-            # Write TC sensor data to DB every 5 mins
+
+            # Write timestamped TC sensor data to DB once every 5 mins
             time_mins = int(time.strftime("%M"))
-            if time_mins % 5 == 0 and not self._db_check:
-                self.db_cur.execute(self.temp_data_sql, (self.thermo_internal_reading, self.thermo_external_reading))
-                self.db_conn.commit()
-                self._db_check = True
+            time_now = time.strftime("%Y-%m-%dT%H:%M:%S")
+            if time_mins % 5 == 0:
+                if self._db_check is False:
+                    self.db_cur.execute(self.temp_data_sql, (self.thermo_internal_reading, 
+                                                             self.thermo_external_reading, 
+                                                             time_now))
+                    self.db_conn.commit()
+                    self._db_check = True
+                    self.logfile.info("Logging date to DB")
 
             # Reset db_check - ensures we only get one value every 5mins
             if time_mins % 5 > 0:
-                self.db_check = False
+                self._db_check = False
                 
-            time.sleep(1)
+            time.sleep(2)
 
 
